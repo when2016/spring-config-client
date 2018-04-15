1d298c146672644015a7a2c89e2699ee696e1ded
wanghongen <whenmm@gmail.com>
2018-04-15 12:49:57 +0800

    'prod'

--- src/main/java/com/howtodoinjava/example/springconfigclient/SpringConfigClientApplication.java
+++ src/main/java/com/howtodoinjava/example/springconfigclient/SpringConfigClientApplication.java
@@ -1,17 +1,41 @@
 package com.howtodoinjava.example.springconfigclient;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringConfigClientApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(SpringConfigClientApplication.class, args);
+  public static void main(String[] args) throws UnknownHostException {
+    //SpringApplication.run(SpringConfigClientApplication.class, args);
+
+    ConfigurableApplicationContext context = SpringApplication
+        .run(SpringConfigClientApplication.class, args);
+
+    Environment env = context.getEnvironment();
+    String protocol = "http";
+    if (env.getProperty("server.ssl.key-store") != null) {
+      protocol = "https";
+    }
+
+    System.out.println(env.getProperty("spring.application.name"));
+    System.out.println(env.getProperty("server.port"));
+    System.out.println(InetAddress.getLocalHost().getHostAddress());
+    System.out.println(env.getProperty("server.port"));
+    System.out.println(env.getActiveProfiles());
+    System.out.println(env.getProperty("spring.profiles.active"));
+    System.out.println(env.getProperty("spring.profiles.active"));
+    System.out.println(env.getProperty("spring.profiles.active"));
+
+
   }
 }
 
